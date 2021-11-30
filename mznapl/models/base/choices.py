582a084cbee00ea4a1e611968dd49f104fa6f3d1
Marzena <marzena.budek@outlook.com>
2021-11-30 21:21:48 +0000

    add post types and consolidate post page styles

--- mznapl/models/base/choices.py
+++ mznapl/models/base/choices.py
@@ -1,7 +1,13 @@
 class PostTag:
     BLOG = 'blog'
     NOTE = 'note'
+    POETRY = 'poetry'
+    RECIPE = 'recipe'
+    ART = 'art'
     CHOICES = (
         (BLOG, 'Blog'),
         (NOTE, 'Note'),
+        (POETRY, 'Poetry'),
+        (RECIPE, 'Recipe'),
+        (ART, 'Art'),
     )
